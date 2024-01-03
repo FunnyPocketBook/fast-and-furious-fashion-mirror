c036a4acc18c58d4149893757b4fd43fc5a12492
Samin137 <asn.samin@gmail.com>
2024-01-03 22:41:22 +0100

    Initial Page + Banner Component + Section

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,41 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div class="app-container">
+    <BannerSection />
+    <HorizontalLine />
+    <!-- Section 1: Energy Consumption -->
+    <ClosetChart />
+    <HorizontalLine />
+    <!-- Section 2: ... -->
+    <!-- Section 3: ... -->
+  </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import HorizontalLine from './components/HorizontalLine.vue';
+import BannerSection from './components/BannerSection.vue'
+import ClosetChart from './components/ClosetChart.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    HorizontalLine,
+    BannerSection,
+    ClosetChart
   }
 }
 </script>
 
 <style>
 #app {
+  font-family: Roboto Mono, Figma Hand, Avenir, Helvetica, Arial, sans-serif;
+}
+
+/* #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
-}
+} */
 </style>
---
+++ src/components/ClosetChart.vue
@@ -0,0 +1,58 @@
+<template>
+    <p>DO YOU <em>REALLY</em> KNOW WHAT IS IN YOUR CLOSET?</p>
+    <div>
+        <div class="tabs">
+            <button v-for="item in tabs" :key="item.title" class="tab" :class="{ active: selectedTab === item.title }"
+                @click="selectTab(item.title)">
+                <img :src="item.imageUrl" :alt="item.title" class="tab-image">
+            </button>
+        </div>
+        <div class="chart" ref="chart"></div>
+    </div>
+</template>
+  
+<script>
+//   import * as d3 from 'd3';
+
+export default {
+    data() {
+        return {
+            tabs: [{ title: "Energy consumption", imageUrl: require('../assets/tab-1.svg') }, { title: "Million metric tons in landfills", imageUrl: require('../assets/tab-2.svg') }],
+            yAxis: ['Cotton', 'Polyester', 'Acrylic'],
+            xAxis: [],
+            selectedTab: 'Cotton',
+        };
+    },
+    methods: {
+        createChart() {
+        },
+        selectTab(tab) {
+            this.selectedTab = tab;
+            // Update the chart based on the selected tab
+            this.updateChart();
+        },
+        updateChart() {
+        },
+    },
+    mounted() {
+        this.createChart();
+    },
+};
+</script>
+  
+<style scoped>
+.tabs {
+}
+
+.tab {
+  background: none;
+  border: none;
+}
+
+.tab.active {
+}
+
+.tab-image {
+}
+</style>
+  
